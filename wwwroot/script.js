801fe0decc2894972235120ec2c1e93164793d44
Luciano Soares <cariocaluciano1805@gmail.com>
2025-03-24 19:29:03 -0300

    ajustes

---
+++ wwwroot/script.js
@@ -0,0 +1,18 @@
+﻿async function ResolvePromisses(promisse, campo) {
+  const resultado = await promisse;  
+  const dados = JSON.parse(resultado);
+  return dados[0][campo]; 
+}
+
+async function UsuarioAdm() {
+  const promisse = Usuario();
+  const resultado = await ResolvePromisses(promisse, "IsAdm");
+  return resultado
+}
+
+async function ModulosLiberados() {
+  const promisse = Usuario();
+  const resultado = await ResolvePromisses(promisse, "ModulosLiberados");
+  console.log(resultado);
+  return resultado;
+}
